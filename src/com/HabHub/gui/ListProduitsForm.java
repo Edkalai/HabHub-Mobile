82c5e94fce0dbc70d041ab66fc3814fff52be60e
aziz <aziz.chniti@esprit.tn>
2022-05-13 09:36:27 +0100

    add to cart

--- src/com/HabHub/gui/ListProduitsForm.java
+++ src/com/HabHub/gui/ListProduitsForm.java
@@ -7,15 +7,20 @@ package com.HabHub.gui;
 
 import com.HabHub.entities.Produit;
 import com.HabHub.services.ServiceProduit;
+import com.codename1.components.ImageViewer;
 import com.codename1.components.InfiniteProgress;
 import com.codename1.components.ScaleImageLabel;
 import com.codename1.components.SpanLabel;
+import com.codename1.io.Log;
+import static com.codename1.io.Log.p;
+import com.codename1.ui.BrowserComponent;
 import com.codename1.ui.Button;
 import com.codename1.ui.ButtonGroup;
 import com.codename1.ui.Component;
 import static com.codename1.ui.Component.BOTTOM;
 import static com.codename1.ui.Component.CENTER;
 import static com.codename1.ui.Component.LEFT;
+import static com.codename1.ui.Component.RIGHT;
 import com.codename1.ui.Container;
 import com.codename1.ui.Dialog;
 import com.codename1.ui.Display;
@@ -28,8 +33,11 @@ import com.codename1.ui.Label;
 import com.codename1.ui.RadioButton;
 import com.codename1.ui.Tabs;
 import com.codename1.ui.TextArea;
+import com.codename1.ui.TextField;
 import com.codename1.ui.Toolbar;
 import com.codename1.ui.URLImage;
+import com.codename1.ui.events.ActionEvent;
+import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.BoxLayout;
 import com.codename1.ui.layouts.FlowLayout;
@@ -40,25 +48,30 @@ import com.codename1.ui.spinner.Picker;
 import com.codename1.ui.util.Resources;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author asus
  */
 public class ListProduitsForm extends BaseForm {
-    
+     Form f = new Form("Panier Liste", new BoxLayout(BoxLayout.Y_AXIS));
       
     Form current;
     public ListProduitsForm(Resources res ) {
+        
+     
+        
+      
           super("Newsfeed",BoxLayout.y()); //herigate men Newsfeed w l formulaire vertical
         Toolbar tb = new Toolbar(true);
         current = this ;
         setToolbar(tb);
         getTitleArea().setUIID("Container");
-        setTitle("Ajout Reclamation");
+        setTitle("Search For A Product");
         getContentPane().setScrollVisible(false);
         
-        
+        int occ = 0;
         tb.addSearchCommand(e ->  {
             
         });
@@ -84,7 +97,7 @@ public class ListProduitsForm extends BaseForm {
         
         
         
-        
+               
          swipe.setUIID("Container");
         swipe.getContentPane().setUIID("Container");
         swipe.hideTabs();
@@ -106,11 +119,14 @@ public class ListProduitsForm extends BaseForm {
         FlowLayout flow = new FlowLayout(CENTER);
         flow.setValign(BOTTOM);
         Container radioContainer = new Container(flow);
+     
+
         for (int iter = 0; iter < rbs.length; iter++) {
             rbs[iter] = RadioButton.createToggle(unselectedWalkthru, bg);
             rbs[iter].setPressedIcon(selectedWalkthru);
             rbs[iter].setUIID("Label");
             radioContainer.add(rbs[iter]);
+            
         }
 
         rbs[0].setSelected(true);
@@ -122,40 +138,84 @@ public class ListProduitsForm extends BaseForm {
 
         Component.setSameSize(radioContainer, s1, s2);
         add(LayeredLayout.encloseIn(swipe, radioContainer));
+        
+        
+        
+        
+        
+
+          
+            ArrayList <Produit> p = new ArrayList<>();
+           
 
         ButtonGroup barGroup = new ButtonGroup();
-        RadioButton mesListes = RadioButton.createToggle("Mes Reclamations", barGroup);
-        mesListes.setUIID("SelectBar");
-        RadioButton liste = RadioButton.createToggle("Autres", barGroup);
-        liste.setUIID("SelectBar");
-        RadioButton partage = RadioButton.createToggle("Reclamer", barGroup);
-        partage.setUIID("SelectBar");
+         RadioButton produits = RadioButton.createToggle("Products", barGroup);
+        produits.setUIID("SelectBar");
+       
+        RadioButton listecom = RadioButton.createToggle("My Cart", barGroup);
+        listecom.setUIID("SelectBar");
+        
+        
+            Container panier = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+        listecom.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                for (int i = 0; i < p.size(); i++) {
+                    Container c3 = new Container(new BoxLayout(BoxLayout.X_AXIS));
+                    Container c4 = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+                    ImageViewer image = new ImageViewer();
+                    Image placeholder = Image.createImage(100, 100, 0xbfc9d2);
+                    EncodedImage encImage = EncodedImage.createFromImage(placeholder, false);
+                    Image img = URLImage.createToStorage(encImage, p.get(i).getImage(), "http://localhost/HabHub-Website/public/FrontOffice/uploads/" + p.get(i).getImage(), URLImage.RESIZE_SCALE);
+                    image.setImage(img);
+                    img.getWidth();
+                    c4.add(image);
+                    Container c5 = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+                    Label lb = new Label(p.get(i).getNom());
+                    Label lb3 = new Label("" + p.get(i).getIdProduit());
+                    Label qt = new Label("" + p.get(i).getNom());
+                    Label lb2 = new Label("Prix :" + p.get(i).getPrix());
+                    TextField nvr = new TextField();
+                    // nvr.setUIID("1");
+                    c5.add(lb);
+                    c5.add(nvr);
+                    c5.add(lb2);
+                    c3.add(c4);
+                    c3.add(c5);
+                    panier.add(c3);
+                }
+                Button Commander = new Button("Commander");
+                Button VoirCommande = new Button("Voir Commande");
+                VoirCommande.setVisible(false);
+                panier.add(Commander);
+                panier.add(VoirCommande);
+
+                f.show();
+            }
+         });
+        
+         f.add(panier);
+     
+          // c1.add(affiche);
+          
         Label arrow = new Label(res.getImage("news-tab-down-arrow.png"), "Container");
 
 
-        mesListes.addActionListener((e) -> {
-               InfiniteProgress ip = new InfiniteProgress();
-        final Dialog ipDlg = ip.showInifiniteBlocking();
-        
-        //  ListReclamationForm a = new ListReclamationForm(res);
-          //  a.show();
-            refreshTheme();
-        });
 
         add(LayeredLayout.encloseIn(
-                GridLayout.encloseIn(3, mesListes, liste, partage),
+                GridLayout.encloseIn(2, listecom, produits),
                 FlowLayout.encloseBottom(arrow)
         ));
 
-        partage.setSelected(true);
+        produits.setSelected(true);
         arrow.setVisible(false);
         addShowListener(e -> {
             arrow.setVisible(true);
-            updateArrowPosition(partage, arrow);
+            updateArrowPosition(produits, arrow);
         });
-        bindButtonSelection(mesListes, arrow);
-        bindButtonSelection(liste, arrow);
-        bindButtonSelection(partage, arrow);
+       
+        bindButtonSelection(listecom, arrow);
+        bindButtonSelection(produits, arrow);
         // special case for rotation
         addOrientationListener(e -> {
             updateArrowPosition(barGroup.getRadioButton(barGroup.getSelectedIndex()), arrow);
@@ -164,8 +224,10 @@ public class ListProduitsForm extends BaseForm {
       
         //Appel affichage methode
         ArrayList<Produit>list = ServiceProduit.getInstance().AfficherProduits();
+             
         
         for(Produit rec : list ) {
+          
              String urlImage = "http://localhost/HabHub-Website/public/FrontOffice/uploads/"+rec.getImage();//image statique pour le moment ba3d taw fi  videos jayin nwarikom image 
             
              Image placeHolder = Image.createImage(120, 90);
@@ -182,11 +244,11 @@ public class ListProduitsForm extends BaseForm {
         }
         
         
-        
+         
     }
     
     
-    
+
     
     
     
@@ -248,6 +310,7 @@ public class ListProduitsForm extends BaseForm {
         l.getParent().repaint();
     }
 
+        
     private void addButton(Image img,Produit rec , Resources res) {
         
         int height = Display.getInstance().convertToPixels(11.5f);
@@ -259,9 +322,9 @@ public class ListProduitsForm extends BaseForm {
         
         
         //kif nzidouh  ly3endo date mathbih fi codenamone y3adih string w y5alih f symfony dateTime w ytab3ni cha3mlt taw yjih
-        Label objetTxt = new Label("nom : "+rec.getNom(),"NewsTopLine2");
-        Label dateTxt = new Label("description : "+rec.getDescription(),"NewsTopLine2");
-        Label etatTxt = new Label("prix : "+rec.getPrix(),"NewsTopLine2" );
+        Label objetTxt = new Label("Name : "+rec.getNom(),"NewsTopLine2");
+        Label dateTxt = new Label("Description : "+rec.getDescription(),"NewsTopLine2");
+        Label etatTxt = new Label("Price : "+rec.getPrix(),"NewsTopLine2" );
         Label marqueTxt = new Label("marque : "+rec.getMarque(),"NewsTopLine2" );
         
         createLineSeparator();
@@ -276,8 +339,10 @@ public class ListProduitsForm extends BaseForm {
         supprmierStyle.setFgColor(0xf21f1f);
         
         FontImage suprrimerImage = FontImage.createMaterial(FontImage.MATERIAL_DELETE, supprmierStyle);
-        lSupprimer.setIcon(suprrimerImage);
+        //lSupprimer.setIcon(suprrimerImage);
         lSupprimer.setTextPosition(RIGHT);
+        
+        
         
         //click delete icon
         lSupprimer.addPointerPressedListener(l -> {
@@ -296,7 +361,7 @@ public class ListProduitsForm extends BaseForm {
                 }
            
         });
-        
+                
         //Update icon 
         Label lModifier = new Label(" ");
         lModifier.setUIID("NewsTopLine");
@@ -304,25 +369,125 @@ public class ListProduitsForm extends BaseForm {
         modifierStyle.setFgColor(0xf7ad02);
         
         FontImage mFontImage = FontImage.createMaterial(FontImage.MATERIAL_MODE_EDIT, modifierStyle);
-        lModifier.setIcon(mFontImage);
+        //lModifier.setIcon(mFontImage);
         lModifier.setTextPosition(LEFT);
         
         
-       /* lModifier.addPointerPressedListener(l -> {
+        Form hi = new Form("Button");
+        Button ap = new Button("Buy");
+        hi.add(ap);
+        ap.addActionListener((e) -> Log.p("Clicked"));
+        
+        
+       /** lModifier.addPointerPressedListener(l -> {
             //System.out.println("hello update");
             new ModifierReclamationForm(res,rec).show();
         });*/
+       
+       ArrayList <Produit> p = new ArrayList<>();
+        ap.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent evt) {
+                    // ap.setUIID("true");
+
+                    if (p.isEmpty()) {
+                        p.add(rec);
+                        Dialog d = new Dialog("Produit Ajoute Avec succés");
+
+                        TextArea popupBody = new TextArea("Produit Ajoute Avec succés");
+                        popupBody.setUIID("PopupBody");
+                        popupBody.setEditable(true);
+                        d.setLayout(new BorderLayout());
+                        d.add(BorderLayout.CENTER, popupBody);
+                        d.show("Panier", "Produit Ajoute Avec succés", "OK", null);
+
+                        System.out.println("ajouta" + rec.getIdProduit());
+                    } else {
+                        int occ = 0;
+                        for (int i = 0; i < p.size(); i++) {
+                            if (p.get(i).getIdProduit() == rec.getIdProduit()) {
+                                occ++;
+                            }
+                        }
+                        if (occ == 0) {
+                            p.add(rec);
+                            System.out.println("tawaajouta " + rec.getIdProduit());
+                            Dialog d = new Dialog("Produit Ajoute Avec succés");
+
+                            TextArea popupBody = new TextArea("Produit Ajoute Avec succés");
+                            popupBody.setUIID("PopupBody");
+                            popupBody.setEditable(true);
+                            d.setLayout(new BorderLayout());
+                            d.add(BorderLayout.CENTER, popupBody);
+                            d.show("Panier", "Produit Ajoute Avec succés", "OK", null);
+                        }
+
+                    }
+
+                }
+
+            });
         
         
+        
+        /*
+        
+         Button affiche = new Button("Afficher Panier");
+           Container c1 = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+          // c1.add(affiche);
+           Container panier = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+          
+        
+         affiche.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                for (int i = 0; i < p.size(); i++) {
+                    Container c3 = new Container(new BoxLayout(BoxLayout.X_AXIS));
+                    Container c4 = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+                    ImageViewer image = new ImageViewer();
+                    Image placeholder = Image.createImage(100, 100, 0xbfc9d2);
+                    EncodedImage encImage = EncodedImage.createFromImage(placeholder, false);
+                    Image img = URLImage.createToStorage(encImage, p.get(i).getImage(), "http://localhost/HabHub-Website/public/FrontOffice/uploads/" + p.get(i).getImage(), URLImage.RESIZE_SCALE);
+                    image.setImage(img);
+                    img.getWidth();
+                    c4.add(image);
+                    Container c5 = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+                    Label lb = new Label(p.get(i).getNom());
+                    Label lb3 = new Label("" + p.get(i).getIdProduit());
+                    Label qt = new Label("" + p.get(i).getNom());
+                    Label lb2 = new Label("Prix :" + p.get(i).getPrix());
+                    TextField nvr = new TextField();
+                    // nvr.setUIID("1");
+                    c5.add(lb);
+                    c5.add(nvr);
+                    c5.add(lb2);
+                    c3.add(c4);
+                    c3.add(c5);
+                    panier.add(c3);
+                }
+                Button Commander = new Button("Commander");
+                Button VoirCommande = new Button("Voir Commande");
+                VoirCommande.setVisible(false);
+                panier.add(Commander);
+                panier.add(VoirCommande);
+
+                
+            }
+         });*/
         cnt.add(BorderLayout.CENTER,BoxLayout.encloseY(
                 
                 BoxLayout.encloseX(objetTxt),
                 BoxLayout.encloseX(dateTxt),
-                BoxLayout.encloseX(etatTxt,lModifier,lSupprimer)));
+                BoxLayout.encloseX(etatTxt,lModifier,lSupprimer,hi)));
         
         
         
         add(cnt);
+              
     }
+                     
+         }
+                 
+
     
-}
+
